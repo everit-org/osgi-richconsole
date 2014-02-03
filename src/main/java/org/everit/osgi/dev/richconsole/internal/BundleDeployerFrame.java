ad6078f33ba0fc89800078a172675bc8fb08f15f
balazs-zsoldos <balazs.zsoldos@everit.biz>
2014-02-03 16:58:18 +0100

    License header and pom.xml update

--- src/main/java/org/everit/osgi/dev/richconsole/internal/BundleDeployerFrame.java
+++ src/main/java/org/everit/osgi/dev/richconsole/internal/BundleDeployerFrame.java
@@ -1,26 +1,20 @@
-package org.everit.osgi.dev.richconsole.internal;
-
-/*
- * Copyright (c) 2011, Everit Kft.
+/**
+ * This file is part of Everit - OSGi Rich Console.
  *
- * All rights reserved.
+ * Everit - OSGi Rich Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * Everit - OSGi Rich Console is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - OSGi Rich Console.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+package org.everit.osgi.dev.richconsole.internal;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
